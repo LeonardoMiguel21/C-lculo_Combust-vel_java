32c74636b3f2913f5a81ca55366262d2d3b54ac0
leoardo <leoardo@LAPTOP-QM2Q8FFT>
2021-08-10 14:21:07 -0300

    Implementanção da Classe Main na execução da Aplicação.

--- CalculoCombustivel/src/main/java/com/leonardo/calculocombustivel/Main.java
+++ CalculoCombustivel/src/main/java/com/leonardo/calculocombustivel/Main.java
@@ -5,10 +5,33 @@
  */
 package com.leonardo.calculocombustivel;
 
+import com.leonardo.calculocombustivel.view.TelaCalculoCombustivel;
+import javax.swing.JDialog;
+import javax.swing.JFrame;
+import javax.swing.JOptionPane;
+import javax.swing.UIManager;
+import javax.swing.UnsupportedLookAndFeelException;
+
 /**
  *
  * @author leoardo
  */
 public class Main {
     
+    
+    public static void main(String[] args) throws UnsupportedLookAndFeelException{
+     lookAndFeel();
+     TelaCalculoCombustivel telaCalcCombustivel = new TelaCalculoCombustivel();
+     telaCalcCombustivel.setVisible(true);
+    }
+    private static void lookAndFeel() throws UnsupportedLookAndFeelException{
+        try {
+            UIManager.setLookAndFeel(UIManager.getSystemLookAndFeelClassName());
+            JFrame.setDefaultLookAndFeelDecorated(true);
+            JDialog.setDefaultLookAndFeelDecorated(true);
+         } catch (ClassNotFoundException | InstantiationException | IllegalAccessException | UnsupportedLookAndFeelException e) {
+            JOptionPane.showMessageDialog(null, e, "Erro", JOptionPane.ERROR_MESSAGE);
+
+        }
+   }
 }
